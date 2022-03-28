8554d237392a85e67df6ce033386430f63093105
Pablo <78887322+monardop@users.noreply.github.com>
2022-03-27 22:18:04 -0300

    Add files via upload
    I start to develop the logic of the application.

---
+++ app.py
@@ -0,0 +1,24 @@
+from tkinter import * 
+from tkinter import messagebox
+import sqlite3
+
+class App():
+    pass
+class LabelFrame(App):
+    pass
+class EntryFrame(App):
+    pass
+class Botones(App):
+    pass
+class BotonCancelar(Botones):
+    pass
+class BotonConfirmar(Botones):
+    pass
+class BotonSalir(Botones):
+    pass
+class Venta():
+    pass
+
+
+if __name__ == "__main__":
+    pass
